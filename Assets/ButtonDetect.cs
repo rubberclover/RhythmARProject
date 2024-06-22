796495335a71f1948ebb72d9beb6c24fe397c677
rubberclover <marlite98@gmail.com>
2024-06-22 12:17:43 +0900

    Delete notes added

--- Assets/ButtonDetect.cs
+++ Assets/ButtonDetect.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e690c33a64c1c35b6b9dd5c39675e37b93664a2cc98ca658de429efd29960ef
-size 686
+oid sha256:3d9a82e3d4f5d2e688da5c116cdc5aadba6bfd925c9a5d8f3ccebfeb30d56482
+size 1001
--- Assets/SpawnNotes.cs
+++ Assets/SpawnNotes.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3f687ffe9261482c9929dfe14aabb6094f82660e370d705c5b146d01cd21880
-size 2105
+oid sha256:d1424a4704d577ec3bdb013aed43a3b8db7a0e3b3792b9057a5d6b14e8518c3e
+size 2353
