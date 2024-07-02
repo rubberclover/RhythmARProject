cda32dce5e0a6467ef5298e434c9678b81d194e6
ChristopheCai <caichristo@cy-tech.fr>
2024-07-02 12:24:24 +0900

    MainMenu script

---
+++ Assets/Menu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c181b0dbeb26ce5720b6dc3a45a96a30597314255046a703aafffe47ca42b1b
+size 437
--- Assets/quit.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4363e9a2b8d9e4307640f42a7a2fc931a2525afdd4d5b5c779673fd8cd055af7
-size 267
